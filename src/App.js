fd50137bdfa49bb284576a39e4265ec4251ff6d5
Anvita Guddeti <anvi.guddeti@gmail.com>
2025-04-15 10:22:02 +0530

    project details

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Nav from "./components/Nav";
+import Routing from "./components/Routing";
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Routing />
+    </>
+  );
+}
+
+export default App;
